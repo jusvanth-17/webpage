7ed7bbb4a0ea33b33a3f6bd007e243657076ff03
unknown <admin@T2NNODE-51.psrengg.ac.in>
2024-03-11 15:29:09 +0530

    Update

---
+++ javascript/index.js
@@ -0,0 +1,15 @@
+// let a=10;
+// if(true){
+//     const a=10;
+//     console.log(a)
+// }
+// var a=10;
+// var b="10";
+// console.log(a===b)
+
+// let a=[1,2,3,4];
+// let b =a.map(i=>i*2);
+// console.log(b)
+// let b=a.map((i,index)=>{console.log(i+" multiply by 2 = "+i*2)});
+ 
+let a=5;
